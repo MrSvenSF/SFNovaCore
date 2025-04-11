38673214f9018acbcaaa997ee760fa43f761cd81
MrSvenSF <mrsvensf@gmail.com>
2025-04-11 02:28:16 +0200

    der start

---
+++ Code/src/main/java/DE/MrSvenSF/SF/SFNovaCore.java
@@ -0,0 +1,17 @@
+package DE.MrSvenSF.SF;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SFNovaCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
